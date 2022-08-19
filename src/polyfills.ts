7414c485d459cece8029e1ae2b330659f357aadb
GhofraneFerchichi <ghofranefer01@gmail.com>
2022-08-19 15:09:18 +0100

    first commit

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -46,7 +46,12 @@
  * Zone JS is required by default for Angular itself.
  */
 import 'zone.js';  // Included with Angular CLI.
-
+import 'core-js/es/typed-array';
+import 'core-js/es/object';
+(window as any).global = window;
+(window as any).process = {
+  env: { DEBUG: undefined },
+};
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
